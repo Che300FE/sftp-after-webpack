e9c9c74389d1b8e27db34b0a8bd25ed4b040eef5
gebin <bge@che300.com>
2018-12-28 13:38:07 +0800

    使用es6-promise-pool改写

--- index.js
+++ index.js
@@ -1,89 +1,171 @@
+let PromisePool = require('es6-promise-pool');
 let Client = require("ssh2-sftp-client");
-let sftp = new Client();
 let fs = require("fs");
-let path = require("path");
+let path = require("path"); 
 
-/**
- *
- * @param {本地文件夹路径} localDir
- * @param {文件夹} dirs
- * @param {文件} files
- */
-function GetFileAndDirList(localDir, dirs, files) {
-    var dir = fs.readdirSync(localDir);
-    for (var i = 0; i < dir.length; i++) {
-        var p = path.join(localDir, dir[i]);
-        var stat = fs.statSync(p);
-        if (stat.isDirectory()) {
-            dirs.push(p);
-            GetFileAndDirList(p, dirs, files);
-        } else {
-            files.push(p);
+class  SFTPUtils {
+    constructor(options){
+        this.poolList = [];
+        this.count = 0;
+        this.options = options;
+        this.concurrency = options.concurrency;
+    }
+
+    putCommand(commandFunc){
+        let me = this;
+        return function(){
+            return new Promise(function (resolve, reject) {
+                let sftp = new Client();
+                sftp.connect(me.options)
+                    .then(() => {
+                        return commandFunc(sftp,resolve);
+                    }) 
+                    .catch(err => {
+                        reject(err);
+                        sftp.end();
+                        console.log(err, "connect err");
+                    });
+            })
         }
     }
-}
 
-function upload(options, localDirs) {
-    let remotePath = options.remotePath;
-    if (/\/$/.test(remotePath)) {
-        remotePath = remotePath.substr(0, remotePath.lastIndexOf("/"));
+    mkdirCommand(dirPath,sftp,resolve){
+        let me = this;
+        return sftp.mkdir(dirPath, true).then(() => {
+                resolve()
+                sftp.end();
+            }).catch(function(err){
+                resolve()
+                sftp.end();
+            });
     }
-    sftp
-        .connect(options)
-        .then(() => {
-            // return sftp.list('/home');
-            sftp.mkdir(remotePath, true);
-            let dirs = [],
-                files = [];
-            GetFileAndDirList(localDirs, dirs, files);
-            for (let i = 0; i < dirs.length; i++) {
-                let dir = dirs[i];
-                dir = dir.substr(localDirs.length).replace(/\\/g, "/");
-                sftp.mkdir(`${remotePath}${dir}`, true);
+    
+    putFileCommand(filePath,remotePath,sftp,resolve){
+        let me = this;
+        return sftp
+            .put(filePath, remotePath, true)
+            .then(() => {
+                console.log(
+                    `uploading file ${
+                        filePath
+                    }  ====> ${remotePath}`
+                ); 
+            }).then(() => {
+                resolve();
+                sftp.end();
+            }).catch(function(){
+                resolve()
+                sftp.end();
+            });
+    }
+    
+    sendFileProducer() {
+        let me = this;
+        if(me.count >= me.poolList.length-1){
+            return null;
+        } else {
+            me.count++;
+            return me.poolList[me.count]();      
+        }
+    }
+
+    GetFileAndDirList(localDir, dirs, files) {
+        let me = this;
+        var dir = fs.readdirSync(localDir);
+        for (var i = 0; i < dir.length; i++) {
+            var p = path.join(localDir, dir[i]);
+            var stat = fs.statSync(p);
+            if (stat.isDirectory()) {
+                dirs.push(p);
+                me.GetFileAndDirList(p, dirs, files);
+            } else {
+                files.push(p);
             }
-            for (let index = 0; index < files.length; index++) {
-                let file = files[index];
-                file = file.substr(localDirs.length).replace(/\\/g, "/");
-                sftp
-                    .put(files[index], `${remotePath}${file}`, true)
-                    .then(() => {
-                        console.log(
-                            `uploading file ${
-                                files[index]
-                            }  ====> ${remotePath}${file}`
-                        );
-                        if (index === files.length - 1) {
-                            sftp.end();
-                        }
-                    });
-            }
-        })
-        .catch(err => {
-            sftp.end();
-            console.log(err, "connect err");
+        }
+    }
+
+    uploadByPromisePool(localDirs){
+        let me = this;
+        let options = me.options;
+        let remotePath = options.remotePath;
+        if (/\/$/.test(remotePath)) {
+            remotePath = remotePath.substr(0, remotePath.lastIndexOf("/"));
+        } 
+        
+        me.poolList.push(me.putCommand(function(sftp,resolve){
+            return me.mkdirCommand(remotePath,sftp,resolve);
+        }))
+        
+        let dirs = [],
+            files = [];
+
+        if(Object.prototype.toString.call(localDirs) === '[object Array]'){
+            localDirs.forEach(item => {
+                me.GetFileAndDirList(item, dirs, files);
+            });
+        } else if(Object.prototype.toString.call(localDirs) === '[object String]'){
+            me.GetFileAndDirList(localDirs, dirs, files);
+        }
+
+        console.log(dirs,files)
+        
+        for (let i = 0; i < dirs.length; i++) {
+            let dir = dirs[i];
+            dir = dir.substr(localDirs.length).replace(/\\/g, "/");
+            // console.log('!!!',`${remotePath}${dir}`)
+            me.poolList.push(me.putCommand(function(sftp,resolve){
+                return me.mkdirCommand(`${remotePath}${dir}`,sftp,resolve);
+            }))
+        }
+    
+        for (let index = 0; index < files.length; index++) {
+            let file = files[index];
+            file = file.substr(localDirs.length).replace(/\\/g, "/");
+            // 
+            me.poolList.push(me.putCommand(function(sftp,resolve){
+                return me.putFileCommand(files[index],`${remotePath}${file}`,sftp,resolve);
+            }))
+        }
+    
+        // The number of promises to process simultaneously.
+        console.log('me.poolList',me.poolList.length)
+    
+        // Create a pool.
+        var pool = new PromisePool(me.sendFileProducer.bind(me), me.concurrency)
+        pool.start().then(function () {
+            console.log({"message":"OK"});  
         });
+    }
 }
 
-function getOptions() {
-    const pageJson = JSON.parse(fs.readFileSync("./package.json"));
-    return pageJson["sftp-config"];
-}
 class SftpAfterWebpack {
     constructor(options) {
         this.options = options;
+        this.pageJson = JSON.parse(fs.readFileSync("./package.json"));
     }
+    
+    getOptions() {
+        let me = this;
+        return me.pageJson["sftp-config"];
+    }
+
     apply(compiler) {
+        let me = this;
         let options = this.options;
         compiler.plugin("done", function(stats) {
             if (!stats.hasErrors()) {
                 if (!options) {
-                    options = getOptions();
+                    options = me.getOptions();
                 }
+                me.sftpUtils = new SFTPUtils(options);
+
                 const localPath =
                     options.localPath || compiler.options.output.path;
-                upload(options, localPath);
+                
+                me.sftpUtils.uploadByPromisePool(localPath);
             }
         });
     }
 }
+
 module.exports = SftpAfterWebpack;
